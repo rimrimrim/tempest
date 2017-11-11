b13beafd3d21426609c11b9235c420b6c1aae8c0
takanori midorikawa <takanorimidorikawa@takanori-no-MacBook-Air.local>
2017-11-11 22:13:12 +0900

    product_routes_fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'localhost.localdomain', port: 3000 }
+  config.serve_static_assets = true
+  config.public_file_server.enabled = true
 end

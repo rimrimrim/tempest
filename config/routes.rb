22823d1520f3098aded33ae249f9a51fd1001f0e
takanori midorikawa <takanorimidorikawa@takanori-no-MacBook-Air.local>
2017-11-11 22:36:23 +0900

    ????

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,5 @@ Rails.application.routes.draw do
    end
 
   resources :titles
-
-  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

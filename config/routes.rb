455e7b8f9e8d0e1b3ed7a44701968f4ef28c1bff
takanori midorikawa <takanorimidorikawa@takanori-no-MacBook-Air.local>
2017-11-11 21:21:29 +0900

    index_fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
 
-  root 'titles#index'
   devise_scope :user do
      get '/users/sign_out' => 'devise/sessions#destroy'
    end
 
   resources :titles
 
-  root to: "home#index"
+  root "titles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

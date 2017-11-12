f199ce58231303e6d605ffcfdff10eb6de6e04ce
takanori midorikawa <takanorimidorikawa@takanori-no-MacBook-Air.local>
2017-11-12 23:55:03 +0900

    profile-page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :titles
 
   root to: "home#index"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
